bd6ceb30bffa220c25c1d1d86712fab14e7f9623
deepanshu-kumar-singh <65824194+deepanshu-kumar-singh@users.noreply.github.com>
2020-05-24 15:22:46 +0530

    Day1

---
+++ program3.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the value of a: ")) 
+b = int(input("Enter the value of b: ")) 
+a = a + b 
+b = a - b 
+a = a - b 
+print("After Swaping")
+print("Value of a =", a)
+print("Value of b =", b)
